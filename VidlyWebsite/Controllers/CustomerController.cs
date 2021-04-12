81ac567e7db04e82662180ccbcd10beee63b13bd
Priyanka Ghiya <piyughiya12@gmail.com>
2021-04-12 16:57:53 -0700

    Part2 Added View For Customers and Movies

---
+++ VidlyWebsite/Controllers/CustomerController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VidlyWebsite.Models;
+
+namespace VidlyWebsite.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            var customers = GetCustomers();
+            return View(customers);
+        }
+
+        public IActionResult GetCustomer(int id)
+        {
+            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return View(customer);
+            }
+        }
+
+        private IEnumerable<Customers> GetCustomers()
+        {
+            return new List<Customers>
+            {
+                new Customers { Id =1 , Name = "BAC"},
+                new Customers { Id =2 , Name = "XYZ"}
+            };
+        }
+    }
+}
---
+++ VidlyWebsite/Controllers/MoviesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VidlyWebsite.Models;
+
+namespace VidlyWebsite.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult Index()
+        {
+            var movies = GetMovies();
+            return View(movies);
+        }
+
+        private IEnumerable<Movies> GetMovies()
+        {
+            return new  List<Movies>
+            {
+                new Movies { Id = 10 ,Name="Gabbar"},
+                new Movies { Id = 20 ,Name = "Sarabjit" }
+
+            };
+        }
+    }
+}
